197036e16a4a73d86505fc0f778e3ce5c0866baa
Deepak <dkart032@gmail.com>
2022-05-14 11:46:52 +0530

    added close button to product images

--- client/src/components/Product/ProductImages.js
+++ client/src/components/Product/ProductImages.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import ReactImageZoom from 'react-image-zoom'
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import './ProductImages.css'
+import { IoCloseSharp } from "react-icons/io5";
 
 function ProductImages({ images }) {
     const thumbnailRef = useRef(null)
     const location = useLocation()
+    const navigate = useNavigate()
     const [image, setImage] = useState(images[0])
-    const [showAllImages, setShowAllImages] = useState(true)
+    const [showAllImages, setShowAllImages] = useState(false)
 
-    const showImages = () => setShowAllImages(true)
     const props = { offset: { vertical: 0, horizontal: 15 }, scale: 1.5 }
+    
+    const showImages = () => {
+        document.querySelector('.nav_top').style.display = "none"
+        setShowAllImages(true)
+    }
+    
+    const hideImages = () => {
+        document.querySelector('.nav_top').style.display = "block"
+        setShowAllImages(false)
+        navigate(-1)
+    }
 
     const showImage = e => {
         e.target.parentElement.parentElement.querySelectorAll('div')
@@ -22,10 +34,11 @@ function ProductImages({ images }) {
 
     useEffect(() => {
         if (location.hash !== "#show-all-images") {
+            document.querySelector('.nav_top').style.display = "block"
             setShowAllImages(false)
         }
     }, [location])
-    
+
     useEffect(() => {
         thumbnailRef.current.querySelector('div').classList.add('border_biege')
     }, [])
@@ -40,6 +53,7 @@ function ProductImages({ images }) {
             </div>
             <div ref={thumbnailRef} className={`thumbnails mt-1 hide_scrollbar ${showAllImages ? 'show_all_images' : ''}`}>
                 {images.map(img => <div><img onClick={showImage} src={img} alt='' /></div>)}
+                <span className='d-none close_icon' onClick={hideImages}><IoCloseSharp color='#fff' size={24} /></span>
             </div>
         </>
     )
